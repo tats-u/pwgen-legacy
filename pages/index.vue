ff4b6f6071ddb033ccef3dbc8081bd5308f88ee9
Tatsunori Uchino <tats.u@live.jp>
2019-08-20 01:14:00 +0900

    Add v-model to weights & char-type switches

--- pages/index.vue
+++ pages/index.vue
@@ -12,15 +12,15 @@ v-app
               v-row
                 v-col(cols="12")
                   v-subheader アルファベット
-                  v-slider(min="0" max="120" thumb-label="always")
+                  v-slider(min="0" max="120" thumb-label="always" v-model="weight_alpha")
                   v-subheader 数字
-                  v-slider(min="0" max="120" thumb-label="always")
+                  v-slider(min="0" max="120" thumb-label="always" v-model="weight_num")
                   v-subheader 記号
-                  v-slider(min="0" max="120" thumb-label="always")
+                  v-slider(min="0" max="120" thumb-label="always" v-model="weight_symbol")
               v-row(align="center" justify="space-around")
-                v-switch(label="大文字")
-                v-switch(label="数字")
-                v-switch(label="記号")
+                v-switch(label="大文字" v-model="uses_upper")
+                v-switch(label="数字" v-model="uses_num")
+                v-switch(label="記号" v-model="uses_symbol")
               v-row(align="center" justify="space-around")
                 v-switch(label="!")
                 v-switch(label='"')
@@ -69,5 +69,21 @@ v-app
                     | AAAAAAA
                     v-btn(icon)
                       v-icon mdi-clipboard-arrow-right
-            
 </template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  data: () => {
+    return {
+      uses_upper: true,
+      uses_num: true,
+      uses_symbol: true,
+      weight_alpha: 60,
+      weight_num: 60,
+      weight_symbol: 60
+    }
+  }
+})
+</script>
