c744babe6c25afe9675a3b0f719d9ee5596c461a
Tatsunori Uchino <tats.u@live.jp>
2019-08-23 00:51:58 +0900

    Remove warning

--- pages/index.vue
+++ pages/index.vue
@@ -89,7 +89,7 @@ v-app
               v-btn(color="primary" type="submit")
                 v-icon(left) mdi-check
                 | OK
-    v-btn(fixed fab bottom right color="green" dark href="https://github.com/tats-u/pwgen/" v-on="on")
+    v-btn(fixed fab bottom right color="green" dark href="https://github.com/tats-u/pwgen/")
       v-icon mdi-github-circle
 </template>
 
