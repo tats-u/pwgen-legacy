ac916516391695930ce77845bf801431f3282fbc
Tatsunori Uchino <tats.u@live.jp>
2020-12-15 13:09:43 +0900

    Add eslint-plugin-prettier-vue & update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: "@typescript-eslint/parser",
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:prettier-vue/recommended',
-    'plugin:nuxt/recommended'
+    "@nuxtjs/eslint-config-typescript",
+    "prettier",
+    "prettier/vue",
+    "plugin:prettier/recommended",
+    "plugin:prettier-vue/recommended",
+    "plugin:nuxt/recommended",
   ],
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ["@typescript-eslint", "prettier"],
   // add your custom rules here
-  rules: {
-  }
+  rules: {},
 }
