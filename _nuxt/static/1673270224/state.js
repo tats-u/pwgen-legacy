fd1ba5eeec9ff98fa86b80d6e672255ba831c819
tats-u <tats-u@users.noreply.github.com>
2023-01-09 13:17:23 +0000

    deploy: 8b52b215d41125af26263ac049e72b90a1295ea8

---
+++ _nuxt/static/1673270224/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpwgen\u002F_nuxt\u002Fstatic\u002F1673270224",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpwgen\u002F",assetsPath:"\u002Fpwgen\u002F_nuxt\u002F",cdnURL:a}}}}(null));
